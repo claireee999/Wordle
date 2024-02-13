cf5bd6a7922aa2a62524f87896fc95c9dfc5f977
claireee999 <105027879+claireee999@users.noreply.github.com>
2024-02-13 04:00:38 -0500

    update

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
   const restart = () => {
     setGuesses([]);
-    setAnswer('');
+    setAnswer('answer','');
     setDisplayPopUp(false);
     setAnswerFound(false);
     setMessage('');
